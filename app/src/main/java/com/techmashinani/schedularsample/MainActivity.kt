8ba400d2195a182d614488d141cec5bfb4a1b278
Kennedy Kariuki <you@example.com>
2019-03-07 09:29:24 +0300

    Schedule job

--- app/src/main/java/com/techmashinani/schedularsample/MainActivity.kt
+++ app/src/main/java/com/techmashinani/schedularsample/MainActivity.kt
@@ -1,12 +1,49 @@
 package com.techmashinani.schedularsample
 
+import android.app.job.JobInfo
+import android.app.job.JobScheduler
+import android.content.ComponentName
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var mJobScheduler: JobScheduler? = null
+     companion object {
+         const val JOB_ID = 0
+     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        mJobScheduler = getSystemService(Context.JOB_SCHEDULER_SERVICE) as JobScheduler
+
+        setup()
+    }
+
+    private fun setup() {
+        btn_schedule.setOnClickListener { scheduleJob() }
+        btn_cancel.setOnClickListener { cancelAll() }
+    }
+
+    private fun scheduleJob() {
+        val serviceName = ComponentName(applicationContext, NotificationJobService::class.java)
+        val jobBuilder = JobInfo.Builder(JOB_ID, serviceName).apply {
+            setRequiredNetworkType(JobInfo.NETWORK_TYPE_UNMETERED)
+        }
+        mJobScheduler?.schedule(jobBuilder.build())
+        Toast.makeText(this, "Job Scheduled, job will run when " +
+                "the constraints are met.", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun cancelAll() {
+        if(mJobScheduler != null) {
+            mJobScheduler?.cancelAll()
+            mJobScheduler = null
+            Toast.makeText(this, "Jobs cancelled", Toast.LENGTH_SHORT).show()
+        }
     }
 }
--- app/src/main/java/com/techmashinani/schedularsample/NotificationJobService.kt
+++ app/src/main/java/com/techmashinani/schedularsample/NotificationJobService.kt
@@ -1,16 +1,66 @@
 package com.techmashinani.schedularsample
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.app.job.JobParameters
 import android.app.job.JobService
+import android.content.Context
+import android.content.Intent
+import android.graphics.Color
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 
 class NotificationJobService : JobService() {
 
+    lateinit var mNotifyManager: NotificationManager
+
+    // notification channed ID
+    companion object {
+        const val PRIMARY_CHANNEL_ID: String = "primary_notification_channel"
+        const val NOTIFICATION_ID = 0
+    }
+
     override fun onStartJob(params: JobParameters?): Boolean {
-        return true
+        createNotificationChannel()
+
+        val pendingIntent = PendingIntent.getActivity(this, 0, Intent(this, MainActivity::class.java),
+            PendingIntent.FLAG_UPDATE_CURRENT)
+        with(NotificationManagerCompat.from(this)){
+            notify(NOTIFICATION_ID, createNotification(pendingIntent).build())
+        }
+        return false
     }
 
     override fun onStopJob(params: JobParameters?): Boolean {
         return true
+    }
+
+    private fun createNotificationChannel() {
+        mNotifyManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        if(android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(PRIMARY_CHANNEL_ID, "Job service notification", NotificationManager.IMPORTANCE_HIGH).apply {
+                enableLights(true)
+                enableVibration(true)
+                lightColor = Color.RED
+                description = "Notifications from job service"
+            }
+
+            mNotifyManager.createNotificationChannel(channel)
+        }
+    }
+
+    private fun createNotification(intent: PendingIntent): NotificationCompat.Builder {
+        return NotificationCompat.Builder(this, PRIMARY_CHANNEL_ID).apply {
+            setContentTitle("Job Service")
+            setContentText("Your job is running")
+            setContentIntent(intent)
+            setSmallIcon(R.drawable.ic_action_renew)
+            priority = NotificationCompat.PRIORITY_HIGH
+            setDefaults(NotificationCompat.DEFAULT_ALL)
+            setAutoCancel(true)
+        }
     }
 
 }
